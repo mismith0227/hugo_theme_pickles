15050010898e0bf84649f48eb4b8029d3f781f9b
mismith0227 <doraepon2216@gmail.com>
2017-08-19 18:09:27 +0900

    add menu

--- src/js/zoom/transition.js
+++ src/js/zoom/transition.js
@@ -5,53 +5,50 @@
  * Copyright 2011-2016 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
+const $ = window.jQuery
 
-+function ($) {
-  'use strict'
+// CSS TRANSITION SUPPORT (Shoutout: http://www.modernizr.com/)
+// ============================================================
 
-  // CSS TRANSITION SUPPORT (Shoutout: http://www.modernizr.com/)
-  // ============================================================
+function transitionEnd () {
+  var el = document.createElement('bootstrap')
 
-  function transitionEnd () {
-    var el = document.createElement('bootstrap')
-
-    var transEndEventNames = {
-      WebkitTransition: 'webkitTransitionEnd',
-      MozTransition: 'transitionend',
-      OTransition: 'oTransitionEnd otransitionend',
-      transition: 'transitionend'
-    }
-
-    for (var name in transEndEventNames) {
-      if (el.style[name] !== undefined) {
-        return { end: transEndEventNames[name] }
-      }
-    }
-
-    return false // explicit for ie8 (  ._.)
+  var transEndEventNames = {
+    WebkitTransition: 'webkitTransitionEnd',
+    MozTransition: 'transitionend',
+    OTransition: 'oTransitionEnd otransitionend',
+    transition: 'transitionend'
   }
 
-  // http://blog.alexmaccaw.com/css-transitions
-  $.fn.emulateTransitionEnd = function (duration) {
-    var called = false
-    var $el = this
-    $(this).one('bsTransitionEnd', function () { called = true })
-    var callback = function () { if (!called) $($el).trigger($.support.transition.end) }
-    setTimeout(callback, duration)
-    return this
+  for (var name in transEndEventNames) {
+    if (el.style[name] !== undefined) {
+      return { end: transEndEventNames[name] }
+    }
   }
 
-  $(function () {
-    $.support.transition = transitionEnd()
+  return false // explicit for ie8 (  ._.)
+}
 
-    if (!$.support.transition) return
+// http://blog.alexmaccaw.com/css-transitions
+$.fn.emulateTransitionEnd = function (duration) {
+  var called = false
+  var $el = this
+  $(this).one('bsTransitionEnd', function () { called = true })
+  var callback = function () { if (!called) $($el).trigger($.support.transition.end) }
+  setTimeout(callback, duration)
+  return this
+}
 
-    $.event.special.bsTransitionEnd = {
-      bindType: $.support.transition.end,
-      delegateType: $.support.transition.end,
-      handle: function (e) {
-        if ($(e.target).is(this)) return e.handleObj.handler.apply(this, arguments)
-      }
+$(function () {
+  $.support.transition = transitionEnd()
+
+  if (!$.support.transition) return
+
+  $.event.special.bsTransitionEnd = {
+    bindType: $.support.transition.end,
+    delegateType: $.support.transition.end,
+    handle: function (e) {
+      if ($(e.target).is(this)) return e.handleObj.handler.apply(this, arguments)
     }
-  })
-}(jQuery)
+  }
+})
