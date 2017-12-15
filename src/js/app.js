93dcb0b7c5e61eeb882c9b5ea868699d557eba12
mismith0227 <doraepon2216@gmail.com>
2017-12-16 02:11:07 +0900

    add slick slider
    #38

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,24 @@
 const $ = window.jQuery
 require('zoom.js')
 require('./zoom/transition.js')
+require('slick-carousel')
 
 $(() => {
   // Add JavaScript Code
+  const relatedItemCount = $('.js-related__item').length
+  console.log(relatedItemCount)
+  if (relatedItemCount < 4) {
+    $('#slider').slick('unslick')
+  } else {
+    $('#slider').slick({
+      slidesToShow: 3,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          centerMode: true
+        }
+      }]
+    })
+  }
 })
