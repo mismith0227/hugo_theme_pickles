bdc69d4c4553de2d930d75a868ccd86a1b45c3f4
mismith0227 <doraepon2216@gmail.com>
2017-12-19 14:22:47 +0900

    edit js
    #38

--- src/js/app.js
+++ src/js/app.js
@@ -4,21 +4,14 @@ require('./zoom/transition.js')
 require('slick-carousel')
 
 $(() => {
-  // Add JavaScript Code
-  const relatedItemCount = $('.js-related__item').length
-  console.log(relatedItemCount)
-  if (relatedItemCount < 4) {
-    $('#slider').slick('unslick')
-  } else {
-    $('#slider').slick({
-      slidesToShow: 3,
-      responsive: [{
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          centerMode: true
-        }
-      }]
-    })
-  }
+  $('#slider').slick({
+    slidesToShow: 3,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true
+      }
+    }]
+  })
 })
